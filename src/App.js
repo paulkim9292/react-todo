15e171c160c55a16a17ff2f58b33ff7b382ba3c1
Sungwoo Kim <paulkim9292@gmail.com>
2022-06-30 22:05:35 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const deleteFromList = (index) => {
+    setToDoList(toDoList.filter((item, toDoIndex) => index !== toDoIndex));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id={styles.page}>
+      <h1>To-Do List</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (toDo === "") {
+            return;
+          }
+          setToDoList((items) => [...items, toDo]);
+          setToDo("");
+        }}
+      >
+        <div id={styles.inputWrapper}>
+          <input
+            autoFocus
+            onChange={(event) => setToDo(event.target.value)}
+            value={toDo}
+          />
+        </div>
+        <button>Add</button>
+      </form>
+      <ul id={styles.toDoList}>
+        {toDoList.map((item, index) => (
+          <li key={index}>
+            <button
+              onClick={() => {
+                deleteFromList(index);
+              }}
+            >
+              ❌
+            </button>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
